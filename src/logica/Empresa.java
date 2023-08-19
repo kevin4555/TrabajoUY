220696bba54727c7159bffea05b09110db7d87a1
KChino <kevintlapizaga@gmail.com>
2023-08-18 22:41:36 -0300

    Update Empresa.java

--- src/logica/Empresa.java
+++ src/logica/Empresa.java
@@ -1,24 +1,54 @@
 package logica;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Empresa extends Usuario {
 	private String descripcion;
 	private String sitioWeb;
+	private ArrayList<OfertaLaboral> ofertasLaborales;
 	
 	public String getDescripcion() {
 		return descripcion;
 	}
+	
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+	
 	public String getSitioWeb() {
 		return sitioWeb;
 	}
+	
 	public void setSitioWeb(String sitioWeb) {
 		this.sitioWeb = sitioWeb;
 	}
 	
 	
 	public void agregarOferta(OfertaLaboral ol) {
-		
+		for (OfertaLaboral oferta : this.ofertasLaborales) {
+			if (ol.getNombre().equals(oferta.getNombre())) {
+				return;
+			}
+		}
+		this.ofertasLaborales.add(ol);
 	}
+	
+	public Set<String> obtenerOfertasEmpresa(){
+		Set<String> ofertas = new HashSet<String>();
+		for (OfertaLaboral oferta : this.ofertasLaborales) {
+			ofertas.add(oferta.getNombre());
+		}
+		return ofertas;
+	}
+
+	public ArrayList<OfertaLaboral> getOfertasLaborales() {
+		return ofertasLaborales;
+	}
+
+	public void setOfertasLaborales(ArrayList<OfertaLaboral> ofertasLaborales) {
+		this.ofertasLaborales = ofertasLaborales;
+	}
+	
 }
