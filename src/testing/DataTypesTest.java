6f547bf4034202cabb0bb7c9eaeb1290cffe1d91
Benjamín Montenegro <benjamin.montenegrom@gmail.com>
2023-08-26 10:43:04 -0300

    testing en datatypes

--- src/testing/DataTypesTest.java
+++ src/testing/DataTypesTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import excepciones.DtOfertaNoExisteException;
 import excepciones.OfertaLaboralYaExisteException;
 import junit.framework.Assert;
+import logica.DataTypes.DTOfertaLaboral;
 import logica.DataTypes.DTPostulante;
 import logica.classes.OfertaLaboral;
 import logica.classes.Postulante;
@@ -41,24 +42,33 @@ public class DataTypesTest {
 	{
 		Postulante postulante = new Postulante("postulante1", "Postulante 1", "Apellido", "postulante@example.com", this.fechaDate, "Nacionalidad");
 		DTPostulante dtPosutlante = postulante.obtenerDTPostulante();
+		Assert.assertEquals(postulante.getNickname(), dtPosutlante.getNickname());
+		Assert.assertEquals(postulante.getNombre(), dtPosutlante.getNombre());
+		Assert.assertEquals(postulante.getApellido(), dtPosutlante.getApellido());
+		Assert.assertEquals(postulante.getEmail(), dtPosutlante.getEmail());
+		Assert.assertEquals(postulante.getNacionalidad(), dtPosutlante.getNacionalidad());
+		Assert.assertEquals(postulante.getFechaNacimiento(), dtPosutlante.getFechaNacimiento());
 	}
 	
 	@Test
 	public void obtenerDTOfertaLaboral() throws OfertaLaboralYaExisteException
 	{
-		ManejadorOfertas manejadorOfertas = ManejadorOfertas.getInstance();
 		TipoPublicacion tipoPublicacion = new TipoPublicacion("tipoPublicacion", "Descripcion", "23", 3, 33.2f,this.fechaDate);
 		OfertaLaboral ofertaLaboral = new OfertaLaboral("Nombre", "Descripcion", "09:00", "19:00", 23.4f, "Ciudad", "Departamento", this.fechaDate, tipoPublicacion);
-		manejadorOfertas.agregarOferta(ofertaLaboral);
+		DTOfertaLaboral dtOfertaLaboral = ofertaLaboral.obtenerDTOfertaLaboral();
+		Assert.assertEquals(ofertaLaboral.getNombre(), dtOfertaLaboral.getNombre());
+		Assert.assertEquals(ofertaLaboral.getRemunaracion(), dtOfertaLaboral.getRemuneracion());
+		Assert.assertEquals(ofertaLaboral.getCiudad(), dtOfertaLaboral.getCiudad());
+		Assert.assertEquals(ofertaLaboral.getDepartamento(), dtOfertaLaboral.getDepartamento());
+		Assert.assertEquals(ofertaLaboral.getDescripcion(), dtOfertaLaboral.getDescripcion());
+		Assert.assertEquals(ofertaLaboral.getHorarioInicial(), dtOfertaLaboral.getHorarioInicio());
+		Assert.assertEquals(ofertaLaboral.getHorarioFinal(), dtOfertaLaboral.getHorarioFinal());
+		Assert.assertEquals(ofertaLaboral.getFechaAlta(), dtOfertaLaboral.getFechaAlta());
+		Assert.assertEquals(ofertaLaboral.getPostulacion().size(), dtOfertaLaboral.getPostulaciones().size());
+	}
+	
+	@Test
+	public void obtenerDTEmpresa() {
 		
-		//Falta la parte de crear el datatype pero no me dio
-		
-		try
-		{
-			manejadorOfertas.obtenerDTOfertaLaboral("OtroNombre");
-		}catch(DtOfertaNoExisteException e)
-		{
-			Assert.assertEquals("No existe la oferta solicitada", e.getMessage());
-		}
 	}
 }
