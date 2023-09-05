563eac212acc1ea58e0de1a2d4f7890550ea1e3c
Cristhian Gomez <cristhian.gomez@fing.edu.uy>
2023-09-04 21:42:45 -0300

    se aplican cambios varios

--- Tarea2/Servidor Central/src/excepciones/PaquetePublicacionYaExisteException.java
+++
@@ -1,8 +0,0 @@
-package excepciones;
-
-@SuppressWarnings("serial")
-public class PaquetePublicacionYaExisteException extends Exception {
-	public PaquetePublicacionYaExisteException(String string) {
-		super(string);
-	}
-}
