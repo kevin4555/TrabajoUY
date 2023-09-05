4d4cb2e09fb6eef33310827e02aa353b6381b814
Cristhian Eduardo Gomez Rodriguez <cristhian.gomez@fing.edu.uy>
2023-09-04 21:22:12 -0300

    Revert "Merge branch 'revert-647cc039' into 'master'"
    This reverts merge request !2

--- Tarea1/Servidor Central/src/excepciones/OfertaLaboralColeccionVaciaException.java
+++
@@ -1,8 +0,0 @@
-package excepciones;
-
-@SuppressWarnings("serial")
-public class OfertaLaboralColeccionVaciaException extends Exception{
-	public OfertaLaboralColeccionVaciaException(String string) {
-		super(string);
-	}
-}
--- Tarea1/Servidor Central/src/excepciones/UsuarioEmailRepetidoException.java
+++
@@ -1,9 +0,0 @@
-package excepciones;
-
-@SuppressWarnings("serial")
-public class UsuarioEmailRepetidoException extends Exception{
-
-	public UsuarioEmailRepetidoException(String message) {
-		super(message);
-	}
-}
