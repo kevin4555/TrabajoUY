a906b047087e1dc020f8de2a080b645ea1dc6266
Valentin Dutra <valentin.dutra@fing.edu.uy>
2023-10-04 13:25:36 -0300

    Revert "encaminado"
    This reverts commit 8cfe319d272cf6c4e3a69bddb49b8855ee162ce0.

--- Tarea2/Servidor-Web/src/main/webapp/WEB-INF/javaScript/ConfirmarFormulario.js
+++
@@ -1,8 +0,0 @@
-
-    function enviarFormulario() {
-        // Envía el formulario
-        document.getElementById('formAltaOferta').submit();
-
-        // Redirige a tu perfil
-        window.location.href = 'url_de_tu_perfil.jsp'; // Reemplaza 'url_de_tu_perfil.jsp' con la URL correcta de tu perfil.
-    }
