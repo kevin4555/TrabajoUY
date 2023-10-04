8cfe319d272cf6c4e3a69bddb49b8855ee162ce0
Valentin Dutra <valentin.dutra@fing.edu.uy>
2023-10-04 13:24:38 -0300

    encaminado

---
+++ Tarea2/Servidor-Web/src/main/webapp/WEB-INF/javaScript/ConfirmarFormulario.js
@@ -0,0 +1,8 @@
+
+    function enviarFormulario() {
+        // Envía el formulario
+        document.getElementById('formAltaOferta').submit();
+
+        // Redirige a tu perfil
+        window.location.href = 'url_de_tu_perfil.jsp'; // Reemplaza 'url_de_tu_perfil.jsp' con la URL correcta de tu perfil.
+    }
