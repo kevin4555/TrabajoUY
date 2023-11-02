0a8b641a8f238a9e5805ead41464f189dd414b4a
dgarcia <alejandrogarcia-20@hotmail.com>
2023-11-01 22:03:28 -0300

    dsa

--- Tarea2/Servidor-Web/src/main/java/controllers/FinalizarOfertaServlet.java
+++ Tarea2/Servidor-Web/src/main/java/controllers/FinalizarOfertaServlet.java
@@ -17,6 +17,8 @@ import logica.webservices.DtPostulacion;
 import logica.webservices.DtPostulante;
 import logica.webservices.DtUsuario;
 import logica.webservices.IOException_Exception;
+import logica.webservices.OfertaLaboralNoExisteException_Exception;
+import logica.webservices.OfertaLaboralNoSePuedeFinalizar_Exception;
 import logica.webservices.PublicadorService;
 import logica.webservices.UsuarioNoExisteException_Exception;
 import model.TipoUsuario;
@@ -43,68 +45,22 @@ public class FinalizarOfertaServlet extends HttpServlet
  private void procesarRequest(HttpServletRequest request, HttpServletResponse response)
      throws ServletException, IOException
    {
-    HttpSession sesion = request.getSession();
-    DtUsuario usuarioLogueado = (DtUsuario) sesion.getAttribute("usuarioLogueado");
+    
+    
     String nombreOferta = request.getParameter("nombreOferta");
     logica.webservices.PublicadorService service = new PublicadorService();
     logica.webservices.Publicador port = service.getPublicadorPort();
-    //port.aceptarRechazarOfertaLaboral(nombreOferta,logica.webservices.EstadoOferta.FINALIZADA, );
+    try {
+      port.finalizarOferta(nombreOferta);
+    } catch (OfertaLaboralNoExisteException_Exception | OfertaLaboralNoSePuedeFinalizar_Exception e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
     
-   /* try
-    {
-     DtUsuario usuario = port.obtenerDtUsuario(nicknameUsuario);
-     request.setAttribute("usuario", usuario);
-
-     if (usuario instanceof DtEmpresa)
-     {
-      ArrayList<DtOfertaLaboral> ofertasConfirmadas = (ArrayList<DtOfertaLaboral>) port
-        .obtenerDtOfertasConfirmadasDeEmpresa(nicknameUsuario).getItem();
-      
-      ArrayList<DtOfertaLaboral> ofertasIngresadas = (ArrayList<DtOfertaLaboral>) port
-        .obtenerDtOfertasIngresadasDeEmpresa(nicknameUsuario).getItem();
-      
-      ArrayList<DtOfertaLaboral> ofertasRechazadas = (ArrayList<DtOfertaLaboral>) port
-        .obtenerDtOfertasRechazadasDeEmpresa(nicknameUsuario).getItem();
-      
-      ArrayList<DtCompraPaquete> compraPaquetes = (ArrayList<DtCompraPaquete>) port
-        .obtenerDtCompraPaqueteDeEmpresa(nicknameUsuario).getItem();
-
-      request.setAttribute("ofertasConfirmadas", ofertasConfirmadas);
-      request.setAttribute("ofertasIngresadas", ofertasIngresadas);
-      request.setAttribute("ofertasRechazadas", ofertasRechazadas);
-      request.setAttribute("compraPaquetes", compraPaquetes);
-      request.setAttribute("tipoUsuario", "empresa");
-     }
-
-     if (usuario instanceof DtPostulante)
-     {
-      ArrayList<DtPostulacion> postulaciones = (ArrayList<DtPostulacion>) port
-        .obtenerDtPostulacionesDePostulante(nicknameUsuario).getItem();
-
-      request.setAttribute("postulaciones", postulaciones);
-      request.setAttribute("tipoUsuario", "postulante");
-     }
-
-    }
-    catch (UsuarioNoExisteException_Exception | IOException_Exception e)
-    {
-     request.getRequestDispatcher("/WEB-INF/error/500.jsp").forward(request, response);
-     e.printStackTrace();
-    }
-
-    if(userAgent != null && userAgent.toLowerCase().contains("mobile"))
-    {
-     if(TipoUsuario.POSTULANTE == sesion.getAttribute("tipoUsuario"))
-      request.getRequestDispatcher("/WEB-INF/mobile/consultas/MiPerfilMobile.jsp").forward(request, response);
-    }
-    else
-    {
-     if (usuarioLogueado != null && nicknameUsuario.equals(usuarioLogueado.getNickname()))
-     {
-      request.getRequestDispatcher("/WEB-INF/consultas/miPerfil.jsp").forward(request, response);
-     }
-     request.getRequestDispatcher("/WEB-INF/consultas/Perfil.jsp").forward(request, response);
-    }*/
+    
+    request.getRequestDispatcher("/WEB-INF/consultas/miPerfil.jsp").forward(request, response);
+    
+   
    }
  
  
