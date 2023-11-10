51a188edbffdcdb1107ab1d6c5a4189b8cd66b33
benjamin.montenegro <benjamin.montenegro@pcunix66.fing.edu.uy>
2023-11-10 15:58:33 -0300

    andando en la fing peñarol peñarol!!

--- Tarea2/Servidor-Web/src/main/java/model/ConfigManager.java
+++ Tarea2/Servidor-Web/src/main/java/model/ConfigManager.java
@@ -1,7 +1,9 @@
 package model;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.FileSystems;
 import java.util.Properties;
 
 /**
@@ -28,12 +30,21 @@ public class ConfigManager {
   }
 
   private void loadProperties() {
-    try {
-      InputStream input = ConfigManager.class
-            .getResourceAsStream("/config.properties");
-
+	  String userHome = System.getProperty("user.home");
+	  String separador = FileSystems.getDefault().getSeparator();
+	  String file = userHome + separador + "config.properties";
+	  try {
+    	InputStream input = new FileInputStream(file);
       properties.load(input);
     } catch (IOException e) {
+    	InputStream input = ConfigManager.class
+      		  .getResourceAsStream("/config.properties");
+    	System.out.println("no econtro archivo");
+    	try {
+			properties.load(input);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
       e.printStackTrace();
     }
   }
