5d7ebdfe6666cc59096f74d74014f91ae7f185eb
Kevin Lapizaga <77381132+kevin4555@users.noreply.github.com>
2023-11-11 02:29:08 -0300

    Fixs

--- Tarea2/Servidor-Web/src/main/java/controllers/CompraPaqueteServlet.java
+++ Tarea2/Servidor-Web/src/main/java/controllers/CompraPaqueteServlet.java
@@ -17,83 +17,66 @@ import main.java.webservices.PublicadorService;
  */
 @WebServlet("/compraPaquete")
 public class CompraPaqueteServlet extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-  /**
-   * @see HttpServlet#HttpServlet()
-   */
-  public CompraPaqueteServlet() {
-    super();
-    // TODO Auto-generated constructor stub
-  }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public CompraPaqueteServlet() {
+		super();
+	}
 
-  private void procesarRequest(HttpServletRequest request,
-        HttpServletResponse response)
-        throws ServletException, IOException {
-    PublicadorService publicadorService =
-          new PublicadorService();
-    main.java.webservices.Publicador port =
-          publicadorService.getPublicadorPort();
-    String nicknameempresa =
-          request.getParameter("nicknameEmpresa");
-    String nombrePaquete =
-          request.getParameter("nombrePaquete");
-    try {
-      ArrayList<String> paquetesNoComprados =
-            (ArrayList<String>) port
-                  .listarPaquetesNoCompradosDeEmpresa(
-                        nicknameempresa)
-                  .getItem();
-      Boolean permitido = false;
-      for (String paquete : paquetesNoComprados) {
-        if (paquete.equals(nombrePaquete)) {
-          permitido = true;
-          break;
-        }
-      }
-      if (permitido) {
-        port.comprarPaquete(nicknameempresa, nombrePaquete,
-              LocalDate.now().toString());
-        String url = request.getContextPath()
-              + "/perfil?nicknameUsuario="
-              + nicknameempresa;
-        response.sendRedirect(url);
-        return;
-      } else {
-        String url = request.getContextPath()
-              + "/paquete?nombrePaquete="
-              + URLEncoder.encode(nombrePaquete, "UTF-8")
-              + "&error=" + URLEncoder
-                    .encode("Paquete ya comprado", "UTF-8");
-        response.sendRedirect(url);
-        return;
-      }
-    } catch (Exception e) {
-      request.setAttribute("error", e.getMessage());
-      request.getRequestDispatcher("/WEB-INF/error/404.jsp")
-            .forward(request, response);
-      e.printStackTrace();
-    }
-  }
+	private void procesarRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PublicadorService publicadorService = new PublicadorService();
+		main.java.webservices.Publicador port = publicadorService.getPublicadorPort();
+		String nicknameempresa = request.getParameter("nicknameEmpresa");
+		String nombrePaquete = request.getParameter("nombrePaquete");
+		try {
+			ArrayList<String> paquetesNoComprados = (ArrayList<String>) port
+					.listarPaquetesNoCompradosDeEmpresa(nicknameempresa).getItem();
+			Boolean permitido = false;
+			for (String paquete : paquetesNoComprados) {
+				if (paquete.equals(nombrePaquete)) {
+					permitido = true;
+					break;
+				}
+			}
+			if (permitido) {
+				port.comprarPaquete(nicknameempresa, nombrePaquete, LocalDate.now().toString());
+				String url = request.getContextPath() + "/perfil?nicknameUsuario=" + nicknameempresa;
+				response.sendRedirect(url);
+				return;
+			} else {
+				String url = request.getContextPath() + "/paquete?nombrePaquete="
+						+ URLEncoder.encode(nombrePaquete, "UTF-8") + "&error="
+						+ URLEncoder.encode("Paquete ya comprado", "UTF-8");
+				response.sendRedirect(url);
+				return;
+			}
+		} catch (Exception e) {
+			request.setAttribute("error", e.getMessage());
+			request.getRequestDispatcher("/WEB-INF/error/404.jsp").forward(request, response);
+			e.printStackTrace();
+		}
+	}
 
-  /**
-   * @see HttpServlet#doGet(HttpServletRequest request,
-   *      HttpServletResponse response)
-   */
-  protected void doGet(HttpServletRequest request,
-        HttpServletResponse response)
-        throws ServletException, IOException {
-    procesarRequest(request, response);
-  }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		procesarRequest(request, response);
+	}
 
-  /**
-   * @see HttpServlet#doPost(HttpServletRequest request,
-   *      HttpServletResponse response)
-   */
-  protected void doPost(HttpServletRequest request,
-        HttpServletResponse response)
-        throws ServletException, IOException {
-    procesarRequest(request, response);
-  }
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		procesarRequest(request, response);
+	}
 
 }
--- Tarea2/Servidor-Web/src/main/java/controllers/SearchServlet.java
+++ Tarea2/Servidor-Web/src/main/java/controllers/SearchServlet.java
@@ -20,107 +20,87 @@ import model.TipoUsuario;
 
 @WebServlet("/search")
 public class SearchServlet extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-  /**
-   * @see HttpServlet#HttpServlet()
-   */
-  public SearchServlet() {
-    super();
-    // TODO Auto-generated constructor stub
-  }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SearchServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-  private static void
-        initSesion(HttpServletRequest request) {
-    HttpSession sesion = request.getSession();
-    PublicadorService publicadorService =
-          new PublicadorService();
-    main.java.webservices.Publicador port =
-          publicadorService.getPublicadorPort();
+	private static void initSesion(HttpServletRequest request) {
+		HttpSession sesion = request.getSession();
+		PublicadorService publicadorService = new PublicadorService();
+		main.java.webservices.Publicador port = publicadorService.getPublicadorPort();
 
-    ArrayList<String> listaKeywords = (ArrayList<
-          String>) port.listarKeywords().getItem();
-    sesion.setAttribute("listaKeywords", listaKeywords);
+		ArrayList<String> listaKeywords = (ArrayList<String>) port.listarKeywords().getItem();
+		sesion.setAttribute("listaKeywords", listaKeywords);
 
-    if (sesion.getAttribute("estadoSesion") == null) {
-      sesion.setAttribute("estadoSesion",
-            EstadoSesion.NO_LOGIN);
-    }
-    if (sesion.getAttribute("tipoUsuario") == null) {
-      sesion.setAttribute("tipoUsuario",
-            TipoUsuario.VISITANTE);
-    }
-  }
+		if (sesion.getAttribute("estadoSesion") == null) {
+			sesion.setAttribute("estadoSesion", EstadoSesion.NO_LOGIN);
+		}
+		if (sesion.getAttribute("tipoUsuario") == null) {
+			sesion.setAttribute("tipoUsuario", TipoUsuario.VISITANTE);
+		}
+	}
 
-  private void procesarRequest(HttpServletRequest request,
-        HttpServletResponse response)
-        throws ServletException, IOException {
-    initSesion(request);
+	private void procesarRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		initSesion(request);
 
-    PublicadorService publicadorService =
-          new PublicadorService();
-    main.java.webservices.Publicador cliente =
-          publicadorService.getPublicadorPort();
-    String toSearch = request.getParameter("toSearch");
+		PublicadorService publicadorService = new PublicadorService();
+		main.java.webservices.Publicador cliente = publicadorService.getPublicadorPort();
+		String toSearch = request.getParameter("toSearch");
 
-    if (toSearch == null) {
-      toSearch = "";
-    }
-    ArrayList<DtOfertaLaboral> dtOfertas;
-    ArrayList<DtEmpresa> dtEmpresas;
-    try {
+		if (toSearch == null) {
+			toSearch = "";
+		}
+		
+		ArrayList<DtOfertaLaboral> dtOfertas;
+		ArrayList<DtEmpresa> dtEmpresas;
+		try {
 
-      dtOfertas = (ArrayList<DtOfertaLaboral>) cliente
-            .buscarOfertas(toSearch).getItem();
-      dtEmpresas = (ArrayList<DtEmpresa>) cliente
-            .buscarEmpresas(toSearch).getItem();
-    } catch (IOException_Exception e) {
-      request.getRequestDispatcher("/WEB-INF/error/500.jsp")
-            .forward(request, response);
-      e.printStackTrace();
-      return;
-    }
+			dtOfertas = (ArrayList<DtOfertaLaboral>) cliente.buscarOfertas(toSearch).getItem();
+			dtEmpresas = (ArrayList<DtEmpresa>) cliente.buscarEmpresas(toSearch).getItem();
+		} catch (IOException_Exception e) {
+			request.getRequestDispatcher("/WEB-INF/error/500.jsp").forward(request, response);
+			e.printStackTrace();
+			return;
+		}
 
-    boolean alfabetico = false;
-    if (request.getParameter("orden") != null
-          && "alfabetico"
-                .equals(request.getParameter("orden"))) {
-      Collections.sort(dtOfertas, Comparator
-            .comparing(DtOfertaLaboral::getNombre));
-      Collections.sort(dtEmpresas,
-            Comparator.comparing(DtEmpresa::getNickname));
-      alfabetico = true;
-    }
+		boolean alfabetico = false;
+		if (request.getParameter("orden") != null && "alfabetico".equals(request.getParameter("orden"))) {
+			Collections.sort(dtOfertas, Comparator.comparing(DtOfertaLaboral::getNombre));
+			Collections.sort(dtEmpresas, Comparator.comparing(DtEmpresa::getNickname));
+			alfabetico = true;
+		}
 
-    request.setAttribute("listaOfertas", dtOfertas);
-    request.setAttribute("listaEmpresas", dtEmpresas);
-    request.setAttribute("toSearch", toSearch);
-    request.setAttribute("alfabetico", alfabetico);
-    request
-          .getRequestDispatcher(
-                "/WEB-INF/consultas/Search.jsp")
-          .forward(request, response);
-  }
+		request.setAttribute("listaOfertas", dtOfertas);
+		request.setAttribute("listaEmpresas", dtEmpresas);
+		request.setAttribute("toSearch", toSearch);
+		request.setAttribute("alfabetico", alfabetico);
+		request.getRequestDispatcher("/WEB-INF/consultas/Search.jsp").forward(request, response);
+	}
 
-  /**
-   * @see HttpServlet#doGet(HttpServletRequest request,
-   *      HttpServletResponse response)
-   */
-  protected void doGet(HttpServletRequest request,
-        HttpServletResponse response)
-        throws ServletException, IOException {
-    procesarRequest(request, response);
-  }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		procesarRequest(request, response);
+	}
 
-  /**
-   * @see HttpServlet#doPost(HttpServletRequest request,
-   *      HttpServletResponse response)
-   */
-  protected void doPost(HttpServletRequest request,
-        HttpServletResponse response)
-        throws ServletException, IOException {
-    // TODO Auto-generated method stub
-    doGet(request, response);
-  }
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 
 }
